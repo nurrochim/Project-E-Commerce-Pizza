70a43208ae1c4c5ac207f9b3adfb20edd52ae995
nurrochim <nurochim@ymail.com>
2017-06-02 21:56:15 +0700

    update

--- eCommerPizza Versi 2/app/src/main/java/com/ecommerce/ecommerpizzas/view/fragment/implement/FragmentAddUser.java
+++ eCommerPizza Versi 2/app/src/main/java/com/ecommerce/ecommerpizzas/view/fragment/implement/FragmentAddUser.java
@@ -1,10 +1,25 @@
 package com.ecommerce.ecommerpizzas.view.fragment.implement;
 
+import android.widget.Button;
+
+import com.ecommerce.ecommerpizzas.R;
 import com.ecommerce.ecommerpizzas.view.fragment.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Asus on 31/05/2017.
  */
 
 public class FragmentAddUser extends BaseFragment {
+    @BindView(R.id.save_user)
+    Button saveUser;
+
+    @Override
+    public void initView() {
+        view = inflater.inflate(R.layout.add_user, container, false);
+        ButterKnife.bind(this, view);
+
+    }
 }
